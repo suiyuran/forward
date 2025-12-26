c932446cb4fd59707fc54587c1a36f9521fdc838
suiyuran <suiyuran@163.com>
2025-12-26 16:35:38 +0800

    update douban

--- widgets/douban.js
+++ widgets/douban.js
@@ -3,10 +3,22 @@ WidgetMetadata = {
   title: "豆瓣",
   description: "获取豆瓣的榜单数据",
   requiredVersion: "0.0.1",
-  version: "1.0.0",
+  version: "1.0.1",
   author: "suiyuran",
   site: "https://github.com/suiyuran/forward",
   modules: [
+    {
+      id: "showing",
+      title: "影院热映",
+      functionName: "showing",
+      params: [],
+    },
+    {
+      id: "hotGaia",
+      title: "豆瓣热门",
+      functionName: "hotGaia",
+      params: [],
+    },
     {
       id: "top250",
       title: "Top250",
@@ -15,6 +27,24 @@ WidgetMetadata = {
     },
   ],
 };
+
+async function showing() {
+  try {
+    const url = "https://dirs.deno.dev/forward/data/douban/showing.json";
+    return (await Widget.http.get(url)).data;
+  } catch (error) {
+    return [];
+  }
+}
+
+async function hotGaia() {
+  try {
+    const url = "https://dirs.deno.dev/forward/data/douban/hot-gaia.json";
+    return (await Widget.http.get(url)).data;
+  } catch (error) {
+    return [];
+  }
+}
 
 async function top250() {
   try {
