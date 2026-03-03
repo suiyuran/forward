45b78830e0d0467795b42dfbf7f3486c0df45895
suiyuran <suiyuran@163.com>
2026-03-03 16:57:27 +0800

    update

--- widgets/vod/ffzy.js
+++ widgets/vod/ffzy.js
@@ -17,7 +17,7 @@ WidgetMetadata = {
   ],
 };
 
-const URL = "https://api.ffzyapi.com/api.php/provide/vod/from/ffm3u8/at/json/";
+const URL = "http://api.ffzyapi.com/api.php/provide/vod/from/ffm3u8/at/json/";
 
 const UA =
   "Mozilla/5.0 (iPhone; CPU iPhone OS 18_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.5 Mobile/15E148 Safari/604.1";
